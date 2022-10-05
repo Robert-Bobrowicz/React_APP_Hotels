fdb74da32c294946b8f4ebbd2b9e5ed5fa3dac13
unknown <r.bobrowicz@live.com>
2022-10-05 10:17:09 +0200

    Hotels

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,15 +1,26 @@
 import { useState } from "react"
+import useAuth from "../../components/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(props) {
 
     // const emailRef = useRef();
+    const [auth, setAuth] = useAuth();
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const submit = (e) => {
         e.preventDefault();
         // console.log(emailRef.current.value);
-        console.log(email, password)
+
+        setTimeout(() => {
+            //login process
+            setAuth(true);
+            navigate('/');
+        }, 500);
+
+        console.log(email, password);
     }
 
     return (
