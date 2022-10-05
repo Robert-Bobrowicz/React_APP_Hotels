5e7be3b089de2aed8fd069766c88dbcfe28a38ef
unknown <r.bobrowicz@live.com>
2022-10-05 09:58:45 +0200

    Hotels

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,27 +1,39 @@
-import { useRef } from "react"
+import { useState } from "react"
 
 export default function Login(props) {
 
-    const emailRef = useRef();
+    // const emailRef = useRef();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const submit = (e) => {
         e.preventDefault();
-        console.log(emailRef.current.value)
+        // console.log(emailRef.current.value);
+        console.log(email, password)
     }
 
     return (
         <div>
             <h2>Login</h2>
-            <form>
+            <form onSubmit={submit}>
                 <div className="form-groupmt-2">
                     <label>Email</label>
-                    <input type="email" ref={emailRef} className="form-control" placeholder="enter your email" />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className="form-control"
+                        placeholder="enter your email" />
                 </div>
                 <div className="form-group mt-2">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="*************" />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        className="form-control" placeholder="*************" />
                 </div>
-                <button className="btn btn-primary mt-2" onClick={submit}>Login</button>
+                <button className="btn btn-primary mt-2">Login</button>
             </form>
         </div>
 
