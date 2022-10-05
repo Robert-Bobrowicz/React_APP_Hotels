95be1b5aad0c1bd704dac05d073899a45f1de786
unknown <r.bobrowicz@live.com>
2022-10-05 09:50:00 +0200

    Hotels

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,17 +1,27 @@
+import { useRef } from "react"
+
 export default function Login(props) {
+
+    const emailRef = useRef();
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(emailRef.current.value)
+    }
+
     return (
         <div>
             <h2>Login</h2>
             <form>
                 <div className="form-groupmt-2">
                     <label>Email</label>
-                    <input type="email" className="form-control" placeholder="enter your email" />
+                    <input type="email" ref={emailRef} className="form-control" placeholder="enter your email" />
                 </div>
                 <div className="form-group mt-2">
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="*************" />
                 </div>
-                <button className="btn btn-primary mt-2">Login</button>
+                <button className="btn btn-primary mt-2" onClick={submit}>Login</button>
             </form>
         </div>
 
