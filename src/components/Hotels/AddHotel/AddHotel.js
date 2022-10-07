8dd63e08978c063e1b75c4dfe12e27181d286ddd
unknown <r.bobrowicz@live.com>
2022-10-07 18:23:30 +0200

    Hotels

--- src/components/Hotels/AddHotel/AddHotel.js
+++ src/components/Hotels/AddHotel/AddHotel.js
@@ -1,7 +1,7 @@
 
 export default function AddHotel(props) {
     return (
-        <div className="card">
+        <div className="card mb-4">
             <div className="card-header">New hotel</div>
             <div className="card-body">
                 <form>
@@ -20,11 +20,27 @@ export default function AddHotel(props) {
                     <div className="invalid-feedback">Error</div>
 
                     <label>Number of rooms</label>
-                    <input
-                        type="text"
-                        className={`form-control ${false ? 'is-invalid' : ''} mb-4`}
-                        placeholder="enter new hotel name" />
+                    <select className="form-control mb-4">
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                    </select>
                     <div className="invalid-feedback">Error</div>
+
+                    <h6>Amenities</h6>
+                    <div className="form-group">
+                        <label className="me-4">TV <input type="checkbox" /></label>
+                        <label className="me-4">WiFi <input type="checkbox" /></label>
+                        <label className="me-4">Private bathroom <input type="checkbox" /></label>
+                        <label className="me-4">Washing machine <input type="checkbox" /></label>
+                    </div>
+
+                    <div className="form-group mt-2">
+                        <label className="me-4">Select picture</label>
+                        <input type="file" />
+                    </div>
                     
                 </form>
             </div>
